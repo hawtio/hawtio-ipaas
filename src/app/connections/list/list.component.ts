96b25096b1c70f5fc68492cbf817b5d7e76ad138
kahboom <rachelyordan@gmail.com>
2017-02-02 15:30:44 -0500

    Update API URL, comment out WIP and logs

--- src/app/connections/list/list.component.ts
+++ src/app/connections/list/list.component.ts
@@ -1,7 +1,7 @@
-import { Component, /*Input,*/ OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Component, Input, OnInit } from '@angular/core';
+//import { Observable } from 'rxjs/Observable';
 
-import { ConnectionStore } from '../../store/connection/connection.store';
+//import { ConnectionStore } from '../../store/connection/connection.store';
 import { Connections } from '../../store/connection/connection.model';
 
 @Component({
@@ -14,28 +14,26 @@ export class ConnectionsListComponent implements OnInit {
   truncateLimit = 80;
   truncateTrail = '…';
 
-  //@Input() connections: Connections;
-  connections: Observable<Connections>;
+  @Input() connections: Connections;
+  //connections: Observable<Connections>;
 
   // filteredConnections: any[] = [];
   //@Input() filteredConnections: Connections;
 
 
-  //@Input() loading: boolean;
-  loading: Observable<boolean>;
+  @Input() loading: boolean;
+  //loading: Observable<boolean>;
 
   //constructor() { }
   // constructor(private filterService: FilterTextService) { }
-  constructor(private store: ConnectionStore) {
-    this.loading = store.loading;
-
+  constructor(/*private store: ConnectionStore*/) {
     //this.loading = store.loading;
-    this.connections = store.list;
+    //this.connections = store.list;
     //this.connections = store.resource;
   }
 
   ngOnInit() {
-    this.store.loadAll();
+    //this.store.loadAll();
   }
 
   /*
