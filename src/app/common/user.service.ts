971eaf7008acc4e1fe44db55a67566d9e294956e
kahboom <rachelyordan@gmail.com>
2017-10-12 14:59:39 -0400

    feat(logout): Add logout support
    First pass-thru at adding logout support. oauthService logout method is not working properly, need a
    way to remove the user token easily.
    
    1035

--- src/app/common/user.service.ts
+++ src/app/common/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { OAuthService } from 'angular-oauth2-oidc-hybrid';
 
 import { User } from '../model';
 
@@ -7,8 +8,15 @@ import { User } from '../model';
 export class UserService {
   private _user = new BehaviorSubject(<User>{});
 
+  constructor(private oauthService: OAuthService) {}
+
+
   get user() {
     return this._user.asObservable();
+  }
+
+  logout() {
+    return this.oauthService.logOut();
   }
 
   setUser(u: User) {
